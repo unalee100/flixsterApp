7027bc38a2727653e4208093f675ff3fee4bf96f
Una Lee <unalee182@gmail.com>
2021-02-26 15:19:34 -0800

    wk 2

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Una Lee on 2/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
